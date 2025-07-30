ddb7db01f17a583a4ceabe53d856a3208860f9a0
avrv063 <avrovgeneral@gmail.com>
2025-07-30 20:31:34 +0700

    Добавил bot.py и requirements.txt

---
+++ iP_checker.py
@@ -0,0 +1,17 @@
+import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+
+bot = telebot.TeleBot("7776420560:AAF39adDJrluTlx54gWS1n0RTjSbRq98am8")  # Вставь сюда свой токен
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = KeyboardButton(
+        text="🔍 Проверить iPhone",
+        web_app=WebAppInfo(url="https://iphone-checklist.onrender.com")  # Сюда — свой URL
+    )
+    markup.add(btn)
+    bot.send_message(message.chat.id, "Привет! Нажми кнопку для запуска чек-листа:", reply_markup=markup)
+
+bot.infinity_polling()
+ 
